5b907cbf37dd079230b54c521df990806ecf7a69
Lazo <lourdes.lazo.s@gmail.com>
2021-07-09 01:58:06 +0200

    Controller sauces

---
+++ backend/controllers/sauces.js
@@ -0,0 +1,40 @@
+const Object = require('../models/Object');
+
+//Infos requis du corps de la requète
+exports.createObject = (req, res, next) => {
+    delete req.body._id;
+    const object = new Object({
+      ...req.body
+    });
+    object.save() //Enregistre dans la base de données
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+};
+
+//Modifie un objet
+exports.modifyObject = (req, res, next) => {
+  Object.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+  .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+  .catch(error => res.status(400).json({ error }));
+};
+
+//Supprime un objet
+exports.deleteObject =  (req, res, next) => {
+  Object.deleteOne({ _id: req.params.id })
+  .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+  .catch(error => res.status(400).json({ error }));
+};
+
+//Recuperation d'un seul objet
+exports.getOneObject = (req, res, next) => {
+  Object.findOne({ _id: req.params.id })
+  .then(object => res.status(200).json(object))
+  .catch(error => res.status(404).json({ error }));
+};
+
+//Recuperation de tous les objets
+exports.getAllObjects = (req, res, next) => { //Url demandé par le frontend
+  Object.find()
+  .then(objects => res.status(200).json(objects))
+  .catch(error => res.status(400).json({ error }));
+};
