dd60f6dd44eb502ceea6c4a2e4cb137eb96356df
Lazo <lourdes.lazo.s@gmail.com>
2021-07-10 20:19:39 +0200

    Changes controller user
    Add crypt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,24 +1,36 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
 const User = require('../models/user');
+const crypt = require('../crypt/crypt');
+const crypto = new crypt();
 
-exports.signup = (req, res, next) => { //cripte le mot de passe
+exports.signup = (req, res, next) => { 
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+  if (regex.test(req.body.password)) {
+    //cripte le mot de passe
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         const user = new User({ //cree le nouveau utilisateur
-            email: req.body.email,
-            password: hash
+            email: crypto.encrypt(req.body.email),
+            password: hash,
         });
         user.save() //enregistre dans la base de donnees
         .then(() => res.status(201).json({ message: 'Utilisateur cree'}))
         .catch(error => res.status(400).json({ error }));
     })
     .catch(error => res.status(500).json({ error }));
+  } 
+  
+  else { res.status(403).json({
+    error: '8 caracteres minimum comportant une majuscule, une minuscule et une chiffre',
+    });
+  }  
 };
 
-exports.login = (req, res, next) => { //on recupere lutilisateur de la base de donne 
-    User.findOne({ email: req.body.email }) 
+exports.login = (req, res, next) => {  
+  const mail = req.body.email;
+  const encrypt = crypto.encrypt(mail);  
+  User.findOne({ email: encrypt }) //on recupere lutilisateur de la base de donne
       .then(user => {
         if (!user) { //si on n-a pas trouve lutilisateur
           return res.status(401).json({ error: 'Utilisateur non trouvé' });
